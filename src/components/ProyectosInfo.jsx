d656a4be313d7b887549d43d5ef1782d3b03f49e
Daniel Plasencia <daniel.plasencia.dlc@gmail.com>
2025-08-09 12:07:26 -0500

    avances portfolio

--- src/components/ProyectosInfo.jsx
+++ src/components/ProyectosInfo.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 
-
-const ProyectosInfo = ({info}) => {
+const ProyectosInfo = ({info, close}) => {
+    const [colortext, setColortext] = useState(info.colortext)
+    const [colorbar, setcolorbar] = useState(info.colorseparate)
 
     const Itemsdetail = ({title,item}) => {
         console.log(item)
@@ -8,12 +10,21 @@ const ProyectosInfo = ({info}) => {
             <>
                 <div className="flex flex-row justify-between w-full">
                     <div className="flex justify-start w-[40%]">
-                        <span className="text-[#4EFEB3] font-bold text-lg">{title} </span>
+                        <span style={{color: `${colortext}`}} className={`mb-2 font-bold text-lg`}>{title} </span>
                     </div>
-                    <div className="flex flex-col justify-start w-[60%]">
-                        {item.map((cate, index) => (
+                    <div className={`flex flex-wrap justify-start w-[60%] p-1 ${title == 'TECNOLOGIAS' ? 'flex-row' : 'flex-col'}`}>
+                        {title != "TECNOLOGIAS" ? <>{item.map((cate, index) => (
                             <span key={index} className="text-white">{cate}</span>
-                        ))}
+                        ))}</> : 
+                        
+                        <>{item.map((cate, index) => (
+                            <div key={index} className={`${cate.class} mb-1 mr-1 flex items-center  w-fit rounded-xl px-3 `}>
+                                {cate.icon}
+                                <span className="ml-2" >{cate.name}</span>
+                            </div>
+                        ))}</> 
+                        }
+                        
 
                     </div>
                 </div>
@@ -25,21 +36,21 @@ const ProyectosInfo = ({info}) => {
     return (
         <>
             <div className="fixed top-0 left-0 w-full h-full flex justify-center items-start bg-[#13151a] bg-opacity-50 z-50">
-                <div className="flex flex-col rounded-lg overflow-hidden shadow-xl w-[80%] md:w-[70%]  bg-gradient-to-b from-[#20232a] to-[#1c1f26]">
+                <div className="flex relative flex-col rounded-lg overflow-hidden shadow-xl w-[80%] md:w-[70%]  bg-gradient-to-b from-[#20232a] to-[#1c1f26]">
                     
                     {/* Imagen en la parte superior */}
                     <div className="w-full">
                         <img 
-                            src="img/TG.jpg" 
+                            src={info.imgdetail || info.img} 
                             alt="Sharlee" 
                             className="w-full h-[300px] object-cover"
                         />
                     </div>
-
+                    <button onClick={close} className="w-auto text-lg absolute text-zinc-600 right-0 m-5"><i class="fa-solid fa-x"></i></button>
                     {/* Contenido de texto abajo */}
                     <div className="p-6 text-start text-white">
-                        <h1 className="text-3xl font-bold text-[#4EFEB3]">{info.title}</h1>
-                        <div className="w-full border-t border-white my-4"></div>
+                        <h1 style={{color: `${colortext}`}} className={`text-3xl font-bold  `}>{info.title}</h1>
+                        <div style={{borderColor: `${colorbar}`}} className={`w-full border-t  my-4`}></div>
 
                         <div className="mt-4 flex justify-between flex-row gap-4">
                             <div className="flex flex-col items-start justify-start w-[50%] ">
@@ -47,11 +58,12 @@ const ProyectosInfo = ({info}) => {
                                 <Itemsdetail title='AÑO' item={['2023']}/>
                                 <Itemsdetail title='EMPRESA' item={['TIGAL GROUP']}/>
                                 <Itemsdetail title='CATEGORIA' item={['APP NODE','INTELIGENCIA ARTIFICIAL']}/>
+                                <Itemsdetail title='TECNOLOGIAS' item={info.tags}/>
 
 
                             </div>
                             <div className="flex w-[50%]">
-                                a
+                                <span>{info.textdes || ''}</span>
                             </div>
                         </div>
                     </div>
--- src/components/Proyectositem.jsx
+++ src/components/Proyectositem.jsx
@@ -2,43 +2,129 @@ import React, { useEffect, useRef, useState } from "react";
 import Isotope from "isotope-layout";
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 import ProyectosInfo from "./ProyectosInfo";
+import ReactIcon from "../icons/React";
+import Tailwind from "../icons/Tailwind";
+import Node from "../icons/Node";
+import Java from "../icons/Java";
+import Mysql from "../icons/Mysql";
+import Astro from "../icons/Astro";
+import Azure from "../icons/Azure";
+import Meta from "../icons/Meta";
+import Aws from "../icons/Aws";
+import PostGress from "../icons/PostGress";
+
+const TAGS = {
+  REACT: {
+    name: "REACT",
+    class: "text-white bg-[#003159]",
+    icon: <ReactIcon/>
+  },
+  TAILWIND: {
+    name: "Tailwind CSS",
+    class: "text-white  bg-[#003159]",
+    icon: <Tailwind/>
+  },
+  NODE: {
+    name: "NODE JS",
+    class: "text-white bg-[#026E00]",
+    icon: <Node />
+  },
+  JAVA: {
+    name: "Java",
+    class: "text-[#CC3738] bg-white",
+    icon: <Java/>
+  },
+  MYSQL: {
+    name: "MySql",
+    class: "text-[#253475] bg-[#E7F9FF]",
+    icon: <Mysql/>
+  },
+  ASTRO: {
+    name: "Astro",
+    class: "text-white bg-[#05081A]",
+    icon: <Astro/>
+  },
+  AZURE: {
+    name: "Azure",
+    class: "text-[#2771B8] bg-white",
+    icon: <Azure/>
+  },
+  AWS: {
+    name: "AWS",
+    class: "text-white bg-[#242938]",
+    icon: <Aws/>
+  },
+  META: {
+    name: "META",
+    class: "text-[#2771B8] bg-white",
+    icon: <Meta/>
+  },
+  POSTGRESS: {
+    name: "PostgreSQL",
+    class: "text-[#2771B8] bg-white",
+    icon: <PostGress/>
+  }
+}
 
 const images = [
   {
     img: "img/BOT.jpg",
     title: "CHAT BOT I.A.",
     empresa: "TIGAL GROUP",
-    type: "Aplicacion"
+    type: "Aplicacion",
+    colortext: "#4EFEB3",
+    tags: [TAGS.NODE, TAGS.META, TAGS.AWS],
+    textdes: "Desarrollé un chatbot con Node.js para una empresa que abarca tres áreas de negocio: licorería, floral y servicios de marketing, diseño y branding. Este sistema, integrado con la API de WhatsApp de Meta, permite gestionar conversaciones de clientes, registrar pedidos en una base de datos y almacenar la información de los usuarios atendidos. El chatbot ofrece atención automatizada según la preferencia del cliente, asegurando respuestas rápidas y eficientes. Además, envía notificaciones en tiempo real a los administradores cuando se concreta una venta o se realiza una reserva, optimizando la gestión y mejorando la experiencia del usuario."
   },
   {
     img: "img/HM.png",
     title: "Sistema Web",
     empresa: "TIGAL GROUP",
-    type: "Web"
+    type: "Web",
+    colortext: "#FC6B03",
+    colorseparate: "#233245",
+    tags: [ TAGS.REACT,TAGS.TAILWIND]
   },
   {
     img: "img/login2.png",
     title: "Sistema Administrativo",
     empresa: "HORIZONTE MEDIC",
-    type: "Aplicación Web"
+    type: "Aplicación Web",
+    colortext: "#FC6B03",
+    colorseparate: "#233245",
+    imgdetail: "img/HM.png",
+    tags: [ TAGS.JAVA,TAGS.AZURE]
   },
   {
     img: "img/Minna.png",
     title: "E-Commerce",
     empresa: "MINNA | K-Beauty",
-    type: "Aplicación Web"
+    type: "Aplicación Web",
+    colortext: "#DE9ED2",
+    colorseparate: "#784D81",
+    tags: [ TAGS.NODE,TAGS.REACT,TAGS.TAILWIND,TAGS.MYSQL,TAGS.AZURE],
+    textdes: `Desarrollé una plataforma de comercio electrónico para una empresa especializada en la venta de productos de belleza, encargándome tanto del backend como del frontend. Utilicé metodologías ágiles para garantizar un desarrollo escalable y eficiente.
+  El sitio permite a los usuarios explorar productos por categoría, agregarlos a un carrito de compras y completar sus pedidos mediante MercadoPago, tras registrarse en la plataforma.
+  Además, incluye un panel de administración donde los encargados pueden gestionar productos (agregar, modificar precios, tamaños, imágenes), ver la lista de usuarios registrados, revisar las compras realizadas y enviar notificaciones personalizadas a los clientes, optimizando la comunicación y fidelización.`
+
   },
   {
     img: "img/TG.jpg",
     title: "Sistema Web",
     empresa: "TIGAL GROUP",
-    type: "Web"
+    type: "Web",
+    colortext: "#4EFEB3",
+    tags: [ TAGS.REACT,TAGS.TAILWIND,TAGS.MYSQL]
   },
   {
     img: "img/Cuispes2.png",
     title: "Aplicación Web",
     empresa: "CUISPES PERÚ",
-    type: "Web / Local"
+    type: "Web / Local",
+    colortext: "#ffcf59",
+    colorseparate: "#784D81",
+    imgdetail: "img/Cuispes-Web.png",
+    tags: [ TAGS.REACT,TAGS.TAILWIND,TAGS.AZURE]
   },
   {
     img: "",
@@ -48,15 +134,21 @@ const images = [
   },
   {
     img: "img/Design-azul.jpg",
-    title: "",
-    empresa: "",
-    type: ""
+    title: "E-Commerce",
+    empresa: "TIGAL GROUP",
+    type: "Aplicación Web",
+    colortext: "#0212E4",
+    tags: [ TAGS.REACT,TAGS.TAILWIND,TAGS.MYSQL]
   },
   {
     img: "img/Proditel.png",
     title: "Sistema Web",
     empresa: "TIGAL GROUP",
-    type: "Web"
+    type: "Web",
+    colortext: "#FC6B03",
+    imgdetail: "img/Proditel-Web.png",
+    tags: [ TAGS.REACT,TAGS.TAILWIND,TAGS.ASTRO,TAGS.AZURE]
+
   },
   
   
@@ -96,7 +188,7 @@ const Proyectositem = () => {
           ))}
         </Masonry>
       </ResponsiveMasonry>
-      {modal && <ProyectosInfo info={info}/>}
+      {modal && <ProyectosInfo info={info} close={() => {setModal(false)}} />}
         
     </div>
   );
